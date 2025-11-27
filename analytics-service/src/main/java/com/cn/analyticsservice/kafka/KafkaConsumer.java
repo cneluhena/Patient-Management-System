1741c3532d59a1d68f0a80a09dd036c5bf2c8894
Chamod Neluhena <cneluhena@gmail.com>
2025-11-27 22:30:49 +0530

    Added Kafka Consumer

---
+++ analytics-service/src/main/java/com/cn/analyticsservice/kafka/KafkaConsumer.java
@@ -0,0 +1,27 @@
+package com.cn.analyticsservice.kafka;
+
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Slf4j
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
+
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            log.info("Received Patient Event: Patient ID - {}, Patient Name - {}", patientEvent.getPatientId(), patientEvent.getName());
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error deserializing the message", e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+    }
+}
